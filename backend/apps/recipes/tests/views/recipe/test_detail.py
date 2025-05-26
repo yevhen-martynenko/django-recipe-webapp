bc7c7339ae4843dee043323b6212a6c8b827c7b9
Yevhen Martynenko <yevhen.martynenko.v@gmail.com>
2025-05-26 18:30:44 +0300

    refactor(tests): extract reusable fixture for verified user with recipe

--- backend/apps/recipes/tests/views/recipe/test_detail.py
+++ backend/apps/recipes/tests/views/recipe/test_detail.py
@@ -6,8 +6,9 @@ from rest_framework import status
 from apps.recipes.models import Recipe, RecipeStatus
 
 
-@pytest.mark.django_db
-def test_recipe_detail_success(client, auth_client, api_recipe_endpoints, generate_recipe_data):
+@pytest.fixture
+def verified_user_with_recipe(auth_client, api_recipe_endpoints, generate_recipe_data):
+    """Helper fixture to create a verified user with a recipe"""
     client, user = auth_client
     user.is_verified = True
     user.save()
@@ -22,47 +23,42 @@ def test_recipe_detail_success(client, auth_client, api_recipe_endpoints, genera
 
     recipe_id = response.json()['recipe']['id']
     recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+
+    return client, user, recipe, recipe_data, recipe_id
+
+
+def assert_recipe_response_success(response, recipe_data, user, recipe_id):
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json().get('detail') == 'Recipe retrieved successfully.'
+
+    recipe = response.json().get('recipe')
+    assert recipe.get('id') == recipe_id
+    assert recipe.get('author') == user.username
+    assert recipe.get('title') == recipe_data.get('title')
+    assert recipe.get('description') == recipe_data.get('description')
+    assert recipe.get('source_url') == recipe_data.get('source_url')
+
+
+@pytest.mark.django_db
+def test_recipe_detail_success(verified_user_with_recipe, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.PUBLISHED
-    recipe.private = False
+    recipe.is_private = False
     recipe.save()
 
     response = client.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
-
-    recipe = response.json().get('recipe')
-    assert recipe.get('id') == recipe_id
-    assert recipe.get('author') == user.username
-    assert recipe.get('title') == recipe_data.get('title')
-    assert recipe.get('description') == recipe_data.get('description')
-    assert recipe.get('source_url') == recipe_data.get('source_url')
-    assert recipe.get('status') == RecipeStatus.PUBLISHED.value
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
 
 
 @pytest.mark.django_db
-def test_recipe_detail_success_by_other_user(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_success_by_other_user(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.PUBLISHED
-    recipe.private = False
+    recipe.is_private = False
     recipe.save()
 
     client_2, user_2 = auth_client_2
@@ -70,82 +66,27 @@ def test_recipe_detail_success_by_other_user(client, auth_client, auth_client_2,
     user_2.save()
 
     response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
-
-    recipe = response.json().get('recipe')
-    assert recipe.get('id') == recipe_id
-    assert recipe.get('author') == user.username
-    assert recipe.get('title') == recipe_data.get('title')
-    assert recipe.get('description') == recipe_data.get('description')
-    assert recipe.get('source_url') == recipe_data.get('source_url')
-    assert recipe.get('status') == RecipeStatus.PUBLISHED.value
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
 
 
 @pytest.mark.django_db
-def test_recipe_detail_failure_by_other_user(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
-
-    client_2, user_2 = auth_client_2
-    user_2.is_verified = True
-    user_2.save()
-
-    response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
-    )
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json().get('detail') == 'No Recipe matches the given query.'
-
-
-@pytest.mark.django_db
-def test_recipe_detail_success_by_unauthenticated_user(client, auth_client, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_success_by_unauthenticated_user(client, verified_user_with_recipe, api_recipe_endpoints):
+    auth_client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.PUBLISHED
-    recipe.private = False
+    recipe.is_private = False
     recipe.save()
 
-    unauthorized_client = Client()
-
-    response = unauthorized_client.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+    response = client.get(
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
 
 
 @pytest.mark.django_db
-def test_recipe_detail_non_existing_slug(client, auth_client, api_recipe_endpoints):
+def test_recipe_detail_non_existing_slug(auth_client, api_recipe_endpoints):
     client, user = auth_client
     user.is_verified = True
     user.save()
@@ -158,22 +99,8 @@ def test_recipe_detail_non_existing_slug(client, auth_client, api_recipe_endpoin
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_banned_recipe(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_banned_recipe(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.is_banned = True
     recipe.save()
@@ -183,57 +110,28 @@ def test_recipe_detail_for_banned_recipe(client, auth_client, auth_client_2, api
     user_2.save()
 
     response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
     assert response.json().get('detail') == 'This recipe has been flagged as inappropriate and is hidden.'
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_banned_recipe_by_owner(client, auth_client, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_banned_recipe_by_owner(verified_user_with_recipe, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.is_banned = True
     recipe.save()
 
     response = client.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_deleted_recipe(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_deleted_recipe(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.delete()
 
@@ -242,29 +140,15 @@ def test_recipe_detail_for_deleted_recipe(client, auth_client, auth_client_2, ap
     user_2.save()
 
     response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json().get('detail') == 'This recipe has been deleted.'
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_draft_recipe(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_draft_recipe(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.DRAFT
     recipe.save()
@@ -274,60 +158,31 @@ def test_recipe_detail_for_draft_recipe(client, auth_client, auth_client_2, api_
     user_2.save()
 
     response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json().get('detail') == 'No Recipe matches the given query.'
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_draft_recipe_by_owner(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_draft_recipe_by_owner(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.DRAFT
     recipe.save()
 
     response = client.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_private_recipe(client, auth_client, auth_client_2, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_private_recipe(verified_user_with_recipe, auth_client_2, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.PUBLISHED
-    recipe.private = True
+    recipe.is_private = True
     recipe.save()
 
     client_2, user_2 = auth_client_2
@@ -335,36 +190,21 @@ def test_recipe_detail_for_private_recipe(client, auth_client, auth_client_2, ap
     user_2.save()
 
     response = client_2.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json().get('detail') == 'No Recipe matches the given query.'
 
 
 @pytest.mark.django_db
-def test_recipe_detail_for_private_recipe_by_owner(client, auth_client, api_recipe_endpoints, generate_recipe_data):
-    client, user = auth_client
-    user.is_verified = True
-    user.save()
-
-    recipe_data = generate_recipe_data()
-    response = client.post(
-        api_recipe_endpoints['create'],
-        recipe_data,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get('detail') == 'Recipe created successfully.'
-
-    recipe_id = response.json()['recipe']['id']
-    recipe = Recipe.objects.get(id=recipe_id)
-    recipe_slug = recipe.slug
+def test_recipe_detail_for_private_recipe_by_owner(verified_user_with_recipe, api_recipe_endpoints):
+    client, user, recipe, recipe_data, recipe_id = verified_user_with_recipe
 
     recipe.status = RecipeStatus.PUBLISHED
-    recipe.private = True
+    recipe.is_private = True
     recipe.save()
 
     response = client.get(
-        api_recipe_endpoints['detail'](recipe_slug),
+        api_recipe_endpoints['detail'](recipe.slug),
     )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json().get('detail') == 'Recipe retrieved successfully.'
+    assert_recipe_response_success(response, recipe_data, user, recipe_id)
